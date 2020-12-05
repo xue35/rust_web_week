f9e3e7b8a928fef12fcb04ff50803351e9275f38
xue35.rpi3 <xue35@icloud.com>
2020-12-05 17:11:56 +0800

    Add head, options into testing.

--- rust-hello/src/main.rs
+++ rust-hello/src/main.rs
@@ -6,9 +6,14 @@
 fn index() -> &'static str {
     "Hello, world!"
 }
+#[options("/")]
+fn index_options() -> &'static str {
+    "No otpion defined."
+}
+
 
 fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index])
+    rocket::ignite().mount("/", routes![index, index_options])
 }
 
 fn main() {
@@ -22,13 +27,43 @@ mod test {
     use rocket::http::Status;
 
     #[test]
-    fn hello_root() {
+    fn get_root() {
         let client = Client::new(rocket()).expect("valid rocket instance");
         let mut response = client.get("/").dispatch();
         assert_eq!(response.status(), Status::Ok);
         assert_eq!(response.body_string(), Some("Hello, world!".into()));
     }
     #[test]
+    fn post_root() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.post("/").dispatch();
+        assert_eq!(response.status(), Status::NotFound);
+    }
+    #[test]
+    fn put_root() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.put("/").dispatch();
+        assert_eq!(response.status(), Status::NotFound);
+    }
+     #[test]
+    fn del_root() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.delete("/").dispatch();
+        assert_eq!(response.status(), Status::NotFound);
+    }
+    #[test]
+    fn head_root() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.head("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
+     #[test]
+    fn option_root() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let response = client.options("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
+       #[test]
     fn hello_nonexist_path() {
         let client = Client::new(rocket()).expect("valid rocket instance");
         let response = client.get("/register").dispatch();
